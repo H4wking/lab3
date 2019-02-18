fcf47ebfd3eb2e75302206b715bcf0420c015b71
H4wking <funnyman20062001@gmail.com>
2019-02-18 23:07:16 +0200

    Initial commit

---
+++ twitter_api.py
@@ -0,0 +1,42 @@
+import urllib.request, urllib.parse, urllib.error
+import twurl
+import json
+import ssl
+
+
+def get_data(json, type):
+    """
+    (str, str) -> dict
+    Return a dictionary containing account names as keys and data of given type.
+    """
+    data = {}
+    for user in json["users"]:
+        data[user["name"]] = user[type]
+    return data
+
+
+def get_json(name):
+    """
+    (str) -> dict
+    Return json containing data of given twitter account.
+    """
+    TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    url = twurl.augment(TWITTER_URL,
+                        {'screen_name': name, 'count': '200'})
+    print('Retrieving', url)
+    connection = urllib.request.urlopen(url, context=ctx)
+    data = connection.read().decode()
+
+    js = json.loads(data)
+    return js
+
+
+if __name__ == "__main__":
+    print('')
+    acct = input('Enter Twitter Account: ')
+    js = get_json(acct)
+    type = str(input("Enter data type you want to get: "))
+    print(get_data(js, type))
